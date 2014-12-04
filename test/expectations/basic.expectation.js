72a0037d01f6f6c87c188067d9651bd62f888b12
Ajay Sabhaney <ajay@mothercreative.com>
2014-12-04 11:09:36 -0700

    Updated swig dependency. Fixes #3

--- test/expectations/basic.expectation.js
+++ test/expectations/basic.expectation.js
@@ -1,7 +1,7 @@
 var tpl = function (_swig,_ctx,_filters,_utils,_fn) {
   var _ext = _swig.extensions,
     _output = "";
-_output += _filters["e"](((typeof _ctx.foo !== "undefined") ? ((typeof _ctx.foo !== "undefined") ? _ctx.foo : "") : ((typeof foo !== "undefined") ? foo : "")));
+_output += _filters["e"]((((typeof _ctx.foo !== "undefined" && _ctx.foo !== null) ? ((typeof _ctx.foo !== "undefined" && _ctx.foo !== null) ? _ctx.foo : "") : ((typeof foo !== "undefined" && foo !== null) ? foo : "")) !== null ? ((typeof _ctx.foo !== "undefined" && _ctx.foo !== null) ? ((typeof _ctx.foo !== "undefined" && _ctx.foo !== null) ? _ctx.foo : "") : ((typeof foo !== "undefined" && foo !== null) ? foo : "")) : "" ));
 
   return _output;
 
